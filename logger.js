cdbcc936f3e6ec6fde82904346f5b8026621042c
yelo <zhihuzeye@gmail.com>
2018-02-26 20:13:40 +0800

    Catching error instead of overriding `onerror`

--- logger.js
+++ logger.js
@@ -9,26 +9,19 @@ function logger (opts, stream) {
   function pino (ctx, next) {
     wrap(ctx.req, ctx.res)
     ctx.log = ctx.request.log = ctx.response.log = ctx.req.log
-    ctx.onerror = catchErr(ctx, ctx.onerror)
-    return next()
+    return next().catch(function (e) {
+      ctx.log.error({
+        res: ctx.res,
+        err: {
+          type: e.constructor.name,
+          message: e.message,
+          stack: e.stack
+        },
+        responseTime: ctx.res.responseTime
+      }, 'request errored')
+      throw e
+    })
   }
   pino.logger = wrap.logger
   return pino
-}
-
-// overriding `onerror` is much faster that using try/catch
-function catchErr (ctx, handler) {
-  return function (e) {
-    if (!e) { return handler.call(ctx, e) }
-    ctx.log.error({
-      res: ctx.res,
-      err: {
-        type: e.constructor.name,
-        message: e.message,
-        stack: e.stack
-      },
-      responseTime: ctx.res.responseTime
-    }, 'request errored')
-    return handler.call(ctx, e)
-  }
 }

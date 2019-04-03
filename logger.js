d138fea73b0a641a426e28623b8b45ddd727bed4
Jonathan Stewmon <jstewmon@gmail.com>
2019-04-03 15:31:39 -0500

    interop with pino-http by setting res.err instead of calling log.error

--- logger.js
+++ logger.js
@@ -10,10 +10,13 @@ function logger (opts, stream) {
     wrap(ctx.req, ctx.res)
     ctx.log = ctx.request.log = ctx.response.log = ctx.req.log
     return next().catch(function (err) {
-      ctx.log.error({ err })
+      if (!('err' in ctx.res)) {
+        ctx.res.err = err
+      }
       throw err
     })
   }
+
   pino.logger = wrap.logger
   return pino
 }

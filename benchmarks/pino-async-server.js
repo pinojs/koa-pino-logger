a6121edc01c175734c9c9e1f125beaab1fae11fc
Matteo Collina <hello@matteocollina.com>
2020-04-24 19:11:52 +0200

    Update bench

--- benchmarks/pino-async-server.js
+++ benchmarks/pino-async-server.js
@@ -2,9 +2,10 @@
 
 var Koa = require('koa')
 var logger = require('../')
+var dest = require('pino').destination({ sync: false, minLength: 4096 })
 
 var app = new Koa()
-app.use(logger({ extreme: true }))
+app.use(logger({}, dest))
 
 app.use((ctx) => {
   ctx.body = 'hello world'

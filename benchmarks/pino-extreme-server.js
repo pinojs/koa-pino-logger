3ca8bcf8241537cf17f791c9d4848048e3582e7b
Matteo Collina <hello@matteocollina.com>
2020-04-24 19:06:14 +0200

    Update
    Fixes #25

--- benchmarks/pino-extreme-server.js
+++ benchmarks/pino-extreme-server.js
@@ -4,7 +4,7 @@ var Koa = require('koa')
 var logger = require('../')
 
 var app = new Koa()
-app.use(logger({extreme: true}))
+app.use(logger({ extreme: true }))
 
 app.use((ctx) => {
   ctx.body = 'hello world'
